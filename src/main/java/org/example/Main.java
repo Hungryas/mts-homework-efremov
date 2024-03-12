e6c5d33e2d4b298c38a59a5743aa5d65b1fc5eb3
Hungryas <hungryas@gmail.com>
2024-03-12 23:11:23 +0300

    feat: update main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.service.CreateAnimalService;
+import org.example.service.impl.CreateAnimalServiceImpl;
+import org.example.service.impl.ZooService;
+
 public class Main {
 
     public static void main(String[] args) {
+        CreateAnimalService zooService = new ZooService();
+        zooService.createAnimals();
+
+        CreateAnimalServiceImpl createAnimalService = new CreateAnimalServiceImpl();
+        createAnimalService.createAnimals();
+        createAnimalService.createAnimals(10);
     }
 }
