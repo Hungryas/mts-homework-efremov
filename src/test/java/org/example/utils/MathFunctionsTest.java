4adce2bdca208f48c2ef01f16af6d9931dd176fb
Hungryas <hungryas@gmail.com>
2024-04-02 08:11:18 +0300

    feat: fix splitRangeForConcurrency()

--- src/test/java/org/example/utils/MathFunctionsTest.java
+++ src/test/java/org/example/utils/MathFunctionsTest.java
@@ -59,7 +59,8 @@ class MathFunctionsTest {
     @DisplayName("Позитивный тест primeNumbersFromRange")
     @CsvSource(quoteCharacter = '"', textBlock = """
             2, 2  , "2"
-            0, 100, "2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97"
+            0, 100, "2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97",
+            100, 300, "101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293"
             """)
     void successPrimeNumbersFromRange(int start, int end, String expectedPrimeNumbersString) {
         List<BigInteger> expectedPrimeNumbers = getBigIntegerListFromString(expectedPrimeNumbersString);
