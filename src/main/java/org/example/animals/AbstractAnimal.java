e25c679671d82b9dad82fce7fecb960844c18127
Hungryas <hungryas@gmail.com>
2024-04-08 04:35:20 +0300

    feat: add AbstractAnimal serializer and deserializer

--- src/main/java/org/example/animals/AbstractAnimal.java
+++ src/main/java/org/example/animals/AbstractAnimal.java
@@ -1,12 +1,27 @@
 package org.example.animals;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.SneakyThrows;
+import org.example.utils.ResultReader;
 import lombok.ToString;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Base64;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 
@@ -37,6 +52,7 @@ public class AbstractAnimal implements Animal {
         this.cost = Double.valueOf(randomNumeric(5));
         this.character = faker.lorem().word();
         this.birthDate = faker.date().birthday(0, 30).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        this.secretInformation = ResultReader.readSecretInformation();
     }
 
     @Override
