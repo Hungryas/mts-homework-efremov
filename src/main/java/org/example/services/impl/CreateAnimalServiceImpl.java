8fd731a725ebe40f7bb7b83898f1babc8ca69304
Hungryas <hungryas@gmail.com>
2024-04-08 04:36:18 +0300

    feat: update CreateAnimalServiceImpl and tests

--- src/main/java/org/example/services/impl/CreateAnimalServiceImpl.java
+++ src/main/java/org/example/services/impl/CreateAnimalServiceImpl.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.example.utils.AnimalHelper.getRandomAnimal;
 
@@ -16,11 +18,13 @@ public class CreateAnimalServiceImpl implements CreateAnimalService {
 
     @Override
     public Map<String, List<AbstractAnimal>> createAnimals() {
+        clearLogData();
         Map<String, List<AbstractAnimal>> animals = new HashMap<>();
         int i = 0;
 
         log.info("Created in 'do-while' cycle: ");
         do {
+            appendLogData(String.valueOf(++i));
             addRandomAnimals(animals);
         } while (++i < 10);
 
@@ -35,6 +39,7 @@ public class CreateAnimalServiceImpl implements CreateAnimalService {
         if (number < 1) {
             throw new IllegalArgumentException("Количество животных должно быть положительным");
         }
+        clearLogData();
         Map<String, List<AbstractAnimal>> animals = new HashMap<>();
 
         log.info("Created in 'for-i' cycle: ");
@@ -49,6 +54,11 @@ public class CreateAnimalServiceImpl implements CreateAnimalService {
         AbstractAnimal animal = getRandomAnimal();
         log.info(animal);
         String animalType = animal.getClass().getSimpleName();
+
+        String animalData = Stream.of(animalType, animal.getName(), animal.getCost(), animal.getBirthDate())
+                .map(String::valueOf)
+                .collect(Collectors.joining(" ", " ", "\n"));
+        appendLogData(animalData);
 
         if (!animals.containsKey(animalType)) {
             animals.put(animalType, new ArrayList<>(List.of(animal)));
--- src/test/java/org/example/services/impl/CreateAnimalServiceImplTest.java
+++ src/test/java/org/example/services/impl/CreateAnimalServiceImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,6 +23,7 @@ class CreateAnimalServiceImplTest {
         Map<String, List<AbstractAnimal>> animals = createAnimalService.createAnimals();
         ArrayList<AbstractAnimal> animalList = collectAllAnimalsToList(animals);
         assertThat(animalList).hasSize(10);
+        checkLogData(animalList);
     }
 
     private ArrayList<AbstractAnimal> collectAllAnimalsToList(Map<String, List<AbstractAnimal>> animals) {
@@ -34,12 +36,28 @@ class CreateAnimalServiceImplTest {
         return animalCollection;
     }
 
+    private void checkLogData(ArrayList<AbstractAnimal> animalList) {
+        List<String> animalLogData = LogDataHelper.readLogData();
+        assertThat(animalList).hasSameSizeAs(animalLogData);
+
+        for (String animalLog : animalLogData) {
+            String[] animalProperties = animalLog.split(" ");
+            boolean isMatchedAnimal = animalList.stream()
+                    .filter(animal -> animal.getClass().getSimpleName().equals(animalProperties[0]))
+                    .allMatch(animal -> Objects.equals(animal.getName(), animalProperties[2]) &&
+                            Objects.equals(animal.getCost().toString(), animalProperties[3]) &&
+                            Objects.equals(animal.getBirthDate().toString(), animalProperties[4]));
+            assertThat(isMatchedAnimal).isTrue();
+        }
+    }
+
     @Test
     @DisplayName("Позитивный тест createAnimals родителя")
     void successCreateAnimalsFromDefault() {
         Map<String, List<AbstractAnimal>> animals = createAnimalService.createAnimalsFromDefault();
         ArrayList<AbstractAnimal> animalList = collectAllAnimalsToList(animals);
         assertThat(animalList).hasSize(10);
+        checkLogData(animalList);
     }
 
     @Test
@@ -49,6 +67,7 @@ class CreateAnimalServiceImplTest {
         Map<String, List<AbstractAnimal>> animals = createAnimalService.createAnimals(number);
         ArrayList<AbstractAnimal> animalList = collectAllAnimalsToList(animals);
         assertThat(animalList).hasSize(number);
+        checkLogData(animalList);
     }
 
     @Test
