83e7cab4dfebafd1a9e8cd820dea20ef5d178c40
Hungryas <hungryas@gmail.com>
2024-04-08 04:36:18 +0300

    feat: update CreateAnimalServiceImpl and tests

--- src/main/java/org/example/service/impl/CreateAnimalServiceImpl.java
+++ src/main/java/org/example/service/impl/CreateAnimalServiceImpl.java
@@ -1,5 +1,6 @@
 package org.example.service.impl;
 
+import lombok.extern.log4j.Log4j2;
 import org.example.animal.AbstractAnimal;
 import org.example.service.CreateAnimalService;
 
@@ -7,21 +8,27 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.example.utils.AnimalHelper.getRandomAnimal;
+import static org.example.utils.LogDataHelper.appendLogData;
+import static org.example.utils.LogDataHelper.clearLogData;
 
+@Log4j2
 public class CreateAnimalServiceImpl implements CreateAnimalService {
 
     @Override
     public Map<String, List<AbstractAnimal>> createAnimals() {
+        clearLogData();
         Map<String, List<AbstractAnimal>> animals = new HashMap<>();
         int i = 0;
 
         do {
+            appendLogData(String.valueOf(++i));
             addRandomAnimals(animals);
-        } while (++i < 10);
-
-        System.out.println("Created in 'do-while' cycle: " + animals);
+        } while (i < 10);
+        log.info("Created in 'do-while' cycle: {}", animals);
 
         return animals;
     }
@@ -34,13 +41,14 @@ public class CreateAnimalServiceImpl implements CreateAnimalService {
         if (number < 1) {
             throw new IllegalArgumentException("Количество животных должно быть положительным");
         }
+        clearLogData();
         Map<String, List<AbstractAnimal>> animals = new HashMap<>();
 
-        for (int i = 0; i < number; i++) {
+        for (int i = 1; i <= number; i++) {
+            appendLogData(String.valueOf(i));
             addRandomAnimals(animals);
         }
-
-        System.out.println("Created in 'for-i' cycle: " + animals);
+        log.info("Created in 'for-i' cycle: {}", animals);
 
         return animals;
     }
@@ -48,6 +56,11 @@ public class CreateAnimalServiceImpl implements CreateAnimalService {
     private void addRandomAnimals(Map<String, List<AbstractAnimal>> animals) {
         AbstractAnimal animal = getRandomAnimal();
         String animalType = animal.getClass().getSimpleName();
+
+        String animalData = Stream.of(animalType, animal.getName(), animal.getCost(), animal.getBirthDate())
+                .map(String::valueOf)
+                .collect(Collectors.joining(" ", " ", "\n"));
+        appendLogData(animalData);
 
         if (!animals.containsKey(animalType)) {
             animals.put(animalType, new ArrayList<>(List.of(animal)));
--- src/test/java/org/example/service/impl/CreateAnimalServiceImplTest.java
+++ src/test/java/org/example/service/impl/CreateAnimalServiceImplTest.java
@@ -1,12 +1,14 @@
 package org.example.service.impl;
 
 import org.example.animal.AbstractAnimal;
+import org.example.utils.LogDataHelper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,8 +22,9 @@ class CreateAnimalServiceImplTest {
     @DisplayName("Позитивный тест createAnimals")
     void successCreateAnimals() {
         Map<String, List<AbstractAnimal>> animals = createAnimalService.createAnimals();
-        ArrayList<AbstractAnimal> objects = collectAllAnimalsToList(animals);
-        assertThat(objects).hasSize(10);
+        ArrayList<AbstractAnimal> animalList = collectAllAnimalsToList(animals);
+        assertThat(animalList).hasSize(10);
+        checkLogData(animalList);
     }
 
     private ArrayList<AbstractAnimal> collectAllAnimalsToList(Map<String, List<AbstractAnimal>> animals) {
@@ -30,16 +33,32 @@ class CreateAnimalServiceImplTest {
         for (List<AbstractAnimal> animalList : animals.values()) {
             animalCollection.addAll(animalList);
         }
-        
+
         return animalCollection;
+    }
+
+    private void checkLogData(ArrayList<AbstractAnimal> animalList) {
+        List<String> animalLogData = LogDataHelper.readLogData();
+        assertThat(animalList).hasSameSizeAs(animalLogData);
+        
+        for (String animalLog : animalLogData) {
+            String[] animalProperties = animalLog.split(" ");
+            boolean isMatchedAnimal = animalList.stream()
+                    .filter(animal -> animal.getClass().getSimpleName().equals(animalProperties[0]))
+                    .allMatch(animal -> Objects.equals(animal.getName(), animalProperties[2]) &&
+                            Objects.equals(animal.getCost().toString(), animalProperties[3]) &&
+                            Objects.equals(animal.getBirthDate().toString(), animalProperties[4]));
+            assertThat(isMatchedAnimal).isTrue();
+        }
     }
 
     @Test
     @DisplayName("Позитивный тест createAnimals родителя")
     void successCAnimalsFromDefault() {
         Map<String, List<AbstractAnimal>> animals = createAnimalService.createAnimalsFromDefault();
-        ArrayList<AbstractAnimal> objects = collectAllAnimalsToList(animals);
-        assertThat(objects).hasSize(10);
+        ArrayList<AbstractAnimal> animalList = collectAllAnimalsToList(animals);
+        assertThat(animalList).hasSize(10);
+        checkLogData(animalList);
     }
 
     @Test
@@ -47,8 +66,9 @@ class CreateAnimalServiceImplTest {
     void successCreateAnimalsWithNumber() {
         int number = ThreadLocalRandom.current().nextInt(1, 10);
         Map<String, List<AbstractAnimal>> animals = createAnimalService.createAnimals(number);
-        ArrayList<AbstractAnimal> objects = collectAllAnimalsToList(animals);
-        assertThat(objects).hasSize(number);
+        ArrayList<AbstractAnimal> animalList = collectAllAnimalsToList(animals);
+        assertThat(animalList).hasSize(number);
+        checkLogData(animalList);
     }
 
     @Test
