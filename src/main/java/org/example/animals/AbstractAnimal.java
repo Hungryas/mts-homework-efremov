50c08b82d8267e22ae56f97515609415cf6f74e2
Hungryas <hungryas@gmail.com>
2024-04-14 21:37:30 +0300

    feat: add @ToString for models

--- src/main/java/org/example/animals/AbstractAnimal.java
+++ src/main/java/org/example/animals/AbstractAnimal.java
@@ -3,9 +3,7 @@ package org.example.animals;
 import com.github.javafaker.Faker;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.SneakyThrows;
 import lombok.ToString;
-import org.example.services.files.impl.ResultReaderImpl;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -29,9 +27,6 @@ public class AbstractAnimal implements Animal {
     @Setter
     protected LocalDate birthDate;
 
-    protected String secretInformation;
-
-    @SneakyThrows
     protected AbstractAnimal() {
         Faker faker = new Faker();
         this.breed = faker.lorem().word();
@@ -39,7 +34,6 @@ public class AbstractAnimal implements Animal {
         this.cost = Double.valueOf(randomNumeric(5));
         this.character = faker.lorem().word();
         this.birthDate = faker.date().birthday(0, 30).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        this.secretInformation = new ResultReaderImpl().readSecretInformation();
     }
 
     @Override
