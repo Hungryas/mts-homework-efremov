32a2ba0806327ed38982af6c1ab6296ebdb1c743
Hungryas <hungryas@gmail.com>
2024-06-13 02:55:04 +0300

    feat: add interceptor

--- src/main/java/org/example/DemoConfig.java
+++ src/main/java/org/example/DemoConfig.java
@@ -1,17 +1,25 @@
 package org.example;
 
+import org.example.interceptors.CustomInterceptor;
 import org.example.services.CreateAnimalService;
 import org.example.services.impl.CreateAnimalServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class DemoConfig {
+public class DemoConfig implements WebMvcConfigurer {
 
     @Bean
     @Scope("prototype")
     CreateAnimalService createAnimalService() {
         return new CreateAnimalServiceImpl();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CustomInterceptor());
     }
 }
